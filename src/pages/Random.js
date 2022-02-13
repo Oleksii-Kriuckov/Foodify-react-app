cf6b98cdaba365733694b95d764df43effc7c5c2
Oleksii Kriuchkov <lexx2109@ukr.net>
2022-02-13 13:36:40 +0200

    fghsh

--- src/pages/Random.js
+++ src/pages/Random.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import axios from 'axios'
-import Main from '../Components/main';
+// import Main from '../Components/main';
 import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Styles from './styles/randomPageStyles.css'
@@ -11,7 +11,7 @@ const Random = () => {
 
   async function fetchRandomRecipe() {
     const responce = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
-    console.log(responce.data.meals[0])
+    // console.log(responce.data.meals[0])
     dispath({ type: "RandomRecipe", payload: responce.data.meals[0] })
   }
 
