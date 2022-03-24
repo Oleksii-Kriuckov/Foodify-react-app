6d529678908ac95368c914828e6974ba3d5d4d6d
Oleksii Kriuchkov <lexx2109@ukr.net>
2022-03-24 14:46:19 +0200

    fix links

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap'
-import { Link, useMatch } from 'react-router-dom';
+import { NavLink, useMatch  } from 'react-router-dom';
 
 const Header = () => {
-        const matchMain = useMatch('/');
         const matchFavour = useMatch('/favourites');
 
-        return <Navbar bg="primary" variant="dark" expand="sm" >
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        return (
+                <Navbar bg="primary" variant="dark" expand="sm" >
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-                <Navbar.Collapse id="basic-navbar-nav">
-                        <Container className='d-flex justify-content-md-center justify-content-sm-start' >
-                                <Link to="/" className={matchMain ? 'active' : null}>Random dish</Link>
-                                <Link to="/favourites" className={matchFavour ? 'active' : null}>Favourites</Link>
-                        </Container>
-                </Navbar.Collapse>
-        </Navbar>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                                <Container className='d-flex justify-content-md-center justify-content-sm-start' >
+                                        <NavLink to="/" >Random dish</NavLink>
+                                        <NavLink to="/favourites" >Favourites</NavLink>
+                                </Container>
+                        </Navbar.Collapse>
+                </Navbar>
+        )
 };
 
 export default Header;
