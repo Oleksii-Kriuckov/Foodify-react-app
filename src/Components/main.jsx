e52d2557478e3e636e7cf1464ec9f3ca73c8c58c
Oleksii Kriuchkov <lexx2109@ukr.net>
2022-05-11 22:13:25 +0300

    fix bugs

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
 import ConfirmModal from './ConfirmModal';
 import PlaceholderImg from './PlaceholderImg';
 
